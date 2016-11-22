9ec0eaf04ac7e44b68f5fffdfd485d9b2f087961
Benson Margulies <benson@basistech.com>
2016-11-21 20:39:48 -0500

    Another repair.

--- httplite/core/src/main/java/org/apache/felix/httplite/servlet/HttpServletRequestImpl.java
+++ httplite/core/src/main/java/org/apache/felix/httplite/servlet/HttpServletRequestImpl.java
@@ -832,6 +832,8 @@ public class HttpServletRequestImpl implements HttpServletRequest
                 return sdf.parse(headerValue).getTime();
             } catch (ParseException e) {
                 //
+            } catch (NumberFormatException nfe) {
+                //
             }
         }
         // if none of them work.
@@ -1079,8 +1081,14 @@ public class HttpServletRequestImpl implements HttpServletRequest
 
             if ( nva.length == 2 )
             {
+                boolean mustDecodeValue = nva[1].indexOf('+') >= 0 || nva[1].indexOf('%') >= 0;
                 //Deprecated method decode() intentionally used for Java 1.3 compatibility.
-                params.put( URLDecoder.decode( nva[0].trim() ), nva[1].trim() );
+                //Tomcat only does this if it sees some evidence of encoding.
+                String val = nva[1].trim();
+                if (mustDecodeValue) {
+                    val = URLDecoder.decode(val);
+                }
+                params.put( URLDecoder.decode( nva[0].trim() ), val );
             }
         }
     }
